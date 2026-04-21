03413c1b039033ba34fdb2880cb0fdd5fbef78e4
M7mdQash <sil.qash@protonmail.com>
2026-04-21 21:08:59 +0300

    fixed posting issue

--- planteer/plants/forms.py
+++ planteer/plants/forms.py
@@ -10,7 +10,7 @@ class PlantForm(forms.ModelForm):
             'about': forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}),
             'used_for': forms.Textarea(attrs={'rows': 4, 'class': 'form-control'}),
             'category': forms.Select(attrs={'class': 'form-select'}),
-            'countries': forms.Select(attrs={'class': 'form-select'}),
+            'countries': forms.SelectMultiple(attrs={'class': 'form-select'}),
 
             'image': forms.ClearableFileInput(attrs={'class': 'form-control'}),
             'is_edible': forms.CheckboxInput(attrs={'class': 'form-check-input'}),
