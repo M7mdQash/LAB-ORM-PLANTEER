ccb787fa8ae47ce3d9916ad9b95fb08d70391e61
M7mdQash <sil.qash@protonmail.com>
2026-04-27 11:54:03 +0300

    making profiles

--- planteer/accounts/urls.py
+++ planteer/accounts/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('signup/', views.sign_up_view, name="sign_up"),
     path('login/', views.sign_in_view, name="log_in"),
     path('logout/', views.log_out, name="log_out"),
+    path('profile/<str:user_id>/', views.profile_view, name="profile_view"),
 ]
 #accounts:log_in
--- planteer/accounts/views.py
+++ planteer/accounts/views.py
@@ -4,6 +4,9 @@ from django.http import HttpRequest, HttpResponse
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.db import IntegrityError, transaction
+
+from .models import Profile
 
 
 # Create your views here.
@@ -11,6 +14,7 @@ def sign_up_view(request):
     
     if request.method == 'POST':
         try:
+            # ============ making user ============
             username = request.POST.get('username')
             email = request.POST.get('email')
             password = request.POST.get('password')
@@ -18,10 +22,18 @@ def sign_up_view(request):
             last_name = request.POST.get('last_name', '')
             new_user = User.objects.create_user(username=username, email=email, password=password, first_name=first_name, last_name=last_name)
             new_user.save()
+            
+            # ============ making profile ============
+            profile = Profile(user=new_user,about=request.POST.get('about'), avatar=request.FILES.get('avatar'))
+            profile.save()
+            
+            # ============ end ============
             print(f"new user -> {new_user.username} has signed up")
             #ts myby needs the views.name one but idk
             return redirect('accounts:log_in')
-        
+        except IntegrityError as e:
+            messages.error(request, "Please choose another username", "alert-danger")
+
         except Exception as e:
             print(e)
     return render(request, 'accounts/signup.html', {})
@@ -49,3 +61,14 @@ def log_out(request):
 
     return redirect(request.GET.get("next", "/"))
 
+def profile_view(request, user_id):
+    
+    try:
+        user = User.objects.get(username=user_id)
+        if not Profile.objects.filter(user=user).first():
+            new_profile = Profile(user=user)
+            new_profile.save() 
+    except Exception as e:
+        print(e)
+        return redirect(request,'main:home')
+    return render(request, 'accounts/profile.html', {"user": user})
